ed72d94f1a002b0accf5f59241fe411832e5a02b
AJSPARK31 <88500955+AJSPARK31@users.noreply.github.com>
2022-08-24 15:31:00 +0530

    Add files via upload

---
+++ WRITE A PYTHON PROGRAM TO CREATE A LAMBDA FUNCTION THAT ADDS 25 TO A GIVEN NUMBER PASSED IN AS AN ARGUMENT..py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[12]:
+
+
+# Write a Python program to create a lambda function that adds 25 to a given number passed in as an argument.
+
+x=lambda x:x+25
+print(x(10))
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
